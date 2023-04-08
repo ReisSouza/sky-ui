3b5cb05a904c54a33ecef49ed4227e365033ef89
Reis Souza <reis.souza@outlook.com.br>
2023-04-08 01:05:00 -0300

    feat:

--- packages/core/src/components/Avatar/styles.ts
+++ packages/core/src/components/Avatar/styles.ts
@@ -7,8 +7,6 @@ export const scaleUp = keyframes({
   '100%': { opacity: '0', width: '500px', height: '500px' },
 })
 
-//
-
 export const ContainerAvatar = styled(AvatarPrimitive.Root, {
   display: 'flex',
   alignItems: 'center',
